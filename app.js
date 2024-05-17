06d451ce0bc063c92d49e6e575327ce1618d6f35
Arisha Ghaffar <160336030+Arishay@users.noreply.github.com>
2024-05-17 04:57:44 -0700

    completed

---
+++ app.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+//Creating a design
+console.log(chalk.bold.cyanBright("\n \t Welcome to __Arisha Ghaffar's__ Word Counter"));
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a Sentence",
+    }
+]);
+//counting words
+let words = answers.sentence.trim().split(" ");
+console.log(chalk.bold.cyanBright("\n \t Words in a Sentence:"));
+console.log(words);
+console.log(chalk.bold(`\n -Words Count: ${chalk.bold.redBright(words.length)}`));
